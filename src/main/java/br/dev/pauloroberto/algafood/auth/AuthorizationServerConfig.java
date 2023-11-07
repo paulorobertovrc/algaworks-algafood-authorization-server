1d5132f72f0b01ce94fc141c4bf0cf9f6c42f260
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-11-07 16:03:19 -0400

    Implementação e configuração de refresh tokens no Authorization Server.

--- src/main/java/br/dev/pauloroberto/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/br/dev/pauloroberto/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.dev.pauloroberto.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                     .withClient("algafood-web")
                         .secret(passwordEncoder.encode("senha123"))
-                        .authorizedGrantTypes("password")
+                        .authorizedGrantTypes("password", "refresh_token")
                         .scopes("write", "read")
                         .accessTokenValiditySeconds(60 * 60 * 6) // Equivalente a 6 horas, sendo que o padrão são 12 horas
+                        .refreshTokenValiditySeconds(60 * 60 * 24 * 7) // Equivalente a 7 dias, sendo que o padrão é 30 dias
                 .and()
                     .withClient("checktoken")
                         .secret(passwordEncoder.encode("check123"));
@@ -36,12 +41,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false); // Desabilita o reuso de refresh tokens
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.checkTokenAccess("isAuthenticated()");
+//        security.checkTokenAccess("isAuthenticated()"); // Padrão
+        security.checkTokenAccess("permitAll()"); // Libera o acesso ao endpoint de validação de tokens
     }
 
 }
