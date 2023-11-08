71c512c9e8366abaf8930691fc06a71695e564ea
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-11-08 10:04:31 -0400

    Implementação e configuração do fluxo authorization code.

--- src/main/java/br/dev/pauloroberto/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/br/dev/pauloroberto/algafood/auth/AuthorizationServerConfig.java
@@ -28,17 +28,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
-                    .withClient("algafood-web")
+                    .withClient("algafood-web") // Password Flow
                         .secret(passwordEncoder.encode("senha123"))
                         .authorizedGrantTypes("password", "refresh_token")
                         .scopes("write", "read")
                         .accessTokenValiditySeconds(60 * 60 * 6) // Equivalente a 6 horas, sendo que o padrão são 12 horas
                         .refreshTokenValiditySeconds(60 * 60 * 24 * 7) // Equivalente a 7 dias, sendo que o padrão é 30 dias
                 .and()
-                    .withClient("analytics")
+                    .withClient("analytics") // Authorization Code Flow // http://localhost:8081/oauth/authorize?response_type=code&client_id=analytics&state=abc&redirect_uri=http://aplicacao-cliente
                         .secret(passwordEncoder.encode("analytics123"))
-                        .authorizedGrantTypes("client_credentials")
+                        .authorizedGrantTypes("authorization_code")
                         .scopes("read")
+                        .redirectUris("http://aplicacao-cliente") // URL de callback
+                .and()
+                    .withClient("faturamento") // Client Credentials Flow
+                        .secret(passwordEncoder.encode("faturamento123"))
+                        .authorizedGrantTypes("client_credentials")
+                        .scopes("write", "read")
                 .and()
                     .withClient("checktoken")
                         .secret(passwordEncoder.encode("check123"));
@@ -53,7 +59,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     }
 
     @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+    public void configure(AuthorizationServerSecurityConfigurer security) {
 //        security.checkTokenAccess("isAuthenticated()"); // Padrão
         security.checkTokenAccess("permitAll()"); // Libera o acesso ao endpoint de validação de tokens
     }
